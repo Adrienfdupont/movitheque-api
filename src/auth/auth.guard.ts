a3237c75ea9dca3ba8c0ddf43569b8d4c58b791f
Adrienfdupont <adrienf.dupont@gmail.com>
2023-12-04 22:17:39 +0100

    added env variables

--- src/auth/auth.guard.ts
+++ src/auth/auth.guard.ts
@@ -4,12 +4,16 @@ import {
   Injectable,
   UnauthorizedException,
 } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 import { JwtService } from '@nestjs/jwt';
 import { Request } from 'express';
 
 @Injectable()
 export class AuthGuard implements CanActivate {
-  constructor(private jwtService: JwtService) {}
+  constructor(
+    private jwtService: JwtService,
+    private configService: ConfigService,
+  ) {}
 
   async canActivate(context: ExecutionContext): Promise<boolean> {
     const request = context.switchToHttp().getRequest();
@@ -18,12 +22,9 @@ export class AuthGuard implements CanActivate {
       throw new UnauthorizedException();
     }
     try {
-      const payload = await this.jwtService.verifyAsync(
-        token,
-        {
-          secret: 'secret',
-        }
-      );
+      const payload = await this.jwtService.verifyAsync(token, {
+        secret: this.configService.get<string>('SECRET'),
+      });
       request['user'] = payload;
     } catch {
       throw new UnauthorizedException();
--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -3,13 +3,15 @@ import { AuthService } from './auth.service';
 import { UsersModule } from '../users/users.module';
 import { JwtModule } from '@nestjs/jwt';
 import { AuthController } from './auth.controller';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
   imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
     UsersModule,
     JwtModule.register({
       global: true,
-      secret: 'secret',
+      secret: process.env.SECRET,
       signOptions: { expiresIn: '60s' },
     }),
   ],
